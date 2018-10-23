54ed58340606fd592718ab442d38d3f3400d878e
Tom Sullivan <tom@msbit.com.au>
2018-10-23 17:30:52 +1100

    General comments

--- wraptr.h
+++ wraptr.h
@@ -15,12 +15,25 @@ private:
   RefCount *refCount;
 
 public:
+  // default constructor declaration
   wraptr();
+
+  // wrapping constructor declaration
   wraptr(T *raw);
+
+  // copy constructor declaration
   wraptr(const wraptr<T> &other);
+
+  // move constructor declaration
   wraptr(wraptr<T> &&other);
+
+  // destructor declaration
   ~wraptr();
+
+  // class member access operator overload
   T *operator->() { return this->raw; }
+
+  // copy assignment operator overload
   wraptr<T> &operator=(const wraptr<T> &other) {
     if (this != &other) {
       if (this->refCount->Release() == 0) {
@@ -33,6 +46,8 @@ public:
     }
     return *this;
   }
+
+  // move assignment operator overload
   wraptr<T> &operator=(wraptr<T> &&other) {
     if (this != &other) {
       if (this->refCount->Release() == 0) {
@@ -48,19 +63,23 @@ public:
   }
 };
 
+// default constructor definition
 template <class T> wraptr<T>::wraptr() : wraptr<T>::wraptr(new T()) {}
 
+// wrapping constructor definition
 template <class T>
 wraptr<T>::wraptr(T *raw) : raw(raw), refCount(new RefCount()) {
   this->refCount->Retain();
 }
 
+// copy constructor definition
 template <class T>
 wraptr<T>::wraptr(const wraptr<T> &other)
     : raw(other.raw), refCount(other.refCount) {
   this->refCount->Retain();
 }
 
+// move constructor definition
 template <class T>
 wraptr<T>::wraptr(wraptr<T> &&other)
     : raw(std::move(other.raw)), refCount(std::move(other.refCount)) {
@@ -68,6 +87,7 @@ wraptr<T>::wraptr(wraptr<T> &&other)
   other.refCount = nullptr;
 }
 
+// destructor definition
 template <class T> wraptr<T>::~wraptr() {
   if (this->refCount != nullptr && this->raw != nullptr &&
       this->refCount->Release() == 0) {
